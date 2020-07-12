51fbc1dd560264992889e5cfb62b77b0f2c4bf78
Pekka Lehtikoski <pekka.lehtikoski@iocafe.org>
2020-07-12 16:21:14 -0400

    xx

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Pekka Lehtikoski, Markku Nissinen'
 author = 'Pekka Lehtikoski, Markku Nissinen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.4'
 
 master_doc = 'index'
 
